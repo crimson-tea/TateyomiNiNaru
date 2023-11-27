da9b35a30e7a6f38b1a8099eabf26c264d659082
crimson-tea <91731135+crimson-tea@users.noreply.github.com>
2023-11-27 16:47:33 +0900

    update: 垂直スクロールバーが表示されないようにする

--- src/content.js
+++ src/content.js
@@ -1,6 +1,6 @@
 const elem = document.getElementById("novel_color");
 elem.style.writingMode = "vertical-rl";
 elem.style.textOrientation = "upright";
-elem.style.overflow = "scroll";
+elem.style.overflowX = "scroll";
 elem.style.width = "100%";
 elem.style.height = "80%";
