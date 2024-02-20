489037206102a81fa3f62d83be86b8cebd43d40f
crimson-tea <91731135+crimson-tea@users.noreply.github.com>
2024-02-20 22:59:34 +0900

    スクロールのアルゴリズムを改善し、スクロールを体感滑らかにする

--- src/content.js
+++ src/content.js
@@ -1,35 +1,49 @@
-function changeScrollDirection() {
+function handleScroll() {
     const scrollTarget = document.querySelector("#novel_color");
     if (!scrollTarget) return;
 
     let interval = null;
+
     let count = -1;
+    let from = 0;
     let targetLeft = 0;
 
     scrollTarget.addEventListener("wheel", (e) => {
         e.preventDefault();
         if (e.deltaY === 0) return;
         if (interval) clearInterval(interval);
-        if (count === -1) targetLeft = scrollTarget.scrollLeft;
-        count = 0;
+        if (count === -100) {
+            from = scrollTarget.scrollLeft;
+            targetLeft = scrollTarget.scrollLeft;
+            count = 1;
+        } else {
+            from = scrollTarget.scrollLeft;
+            count = 1;
+        }
 
-        const { scrollLeft, scrollWidth, clientWidth } = scrollTarget;
+        const { scrollWidth, clientWidth } = scrollTarget;
         targetLeft = Math.max(clientWidth - scrollWidth, Math.min(targetLeft - e.deltaY, 1));
 
-        const sign = Math.sign(targetLeft - scrollLeft);
-        const diff = Math.abs(targetLeft - scrollLeft);
+        const sign = Math.sign(targetLeft - from);
+        const diff = Math.abs(targetLeft - from);
         if (sign === 0) return;
-        const scrollDelta = sign * Math.ceil(diff / 30);
 
         interval = setInterval(() => {
-            scrollTarget.scrollLeft += scrollDelta;
+            const newScrollLeft = sign * Math.ceil(diff * easeOutSine(count / 30)) + from;
+
+            scrollTarget.scrollLeft = newScrollLeft;
             count++;
             if (count >= 30) {
                 clearInterval(interval);
-                count = -1;
+                from = targetLeft;
+                count = -100;
             }
         }, 7);
     }, { passive: false });
 }
 
-changeScrollDirection();
+function easeOutSine(x) {
+    return Math.sin((x * Math.PI) / 2);
+}
+
+handleScroll();
